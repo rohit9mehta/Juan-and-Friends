bdd00cd95cd7083a6de84c509938c5b32181a151
Rohit Mehta <rohit9mehta@gmail.com>
2020-01-13 12:26:42 -0800

    spanish implemented

---
+++ scraper.py
@@ -0,0 +1,56 @@
+import requests
+from bs4 import BeautifulSoup
+#import smtplib
+import time
+#import base64
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+import pandas as pd
+
+
+chrome_options = Options()
+chrome_options.add_argument('--headless')
+chrome_options.add_argument('--no-sandbox')
+chrome_options.add_argument('--disable-dev-shm-usage')
+
+driver = webdriver.Chrome(executable_path=r"/Users/rohitm/Downloads/chromedriver", chrome_options=chrome_options)
+#address = '1415 Owl Point'
+#address_bytes = address.encode("utf-8")
+#ubereatsURL = 'https://www.ubereats.com/en-US/search/feed/?pl=' + str(base64.b64encode(address_bytes)) + '&q=' + 'American'
+#print(ubereatsURL)
+#latitude = int(38.46928024)
+#longitude = int(-122.64401246)
+#cuisine = 'Salads'
+#seamlessURL = 'https://www.seamless.com/search?orderMethod=delivery&locationMode=DELIVERY&facetSet=umamiV2&pageSize=20&hideHateos=true&searchMetrics=true&latitude=' + str(latitude) + '&longitude=' + str(longitude) + '&preciseLocation=true&facet=cuisine%3A' + cuisine + '&sortSetId=umamiv3&countOmittingTimes=true&sponsoredSize=3'
+#deliveryURL = 'https://www.ebay.com/itm/Canon-EOS-5D-Mark-IV-Digital-SLR-Camera-Body-Only/323086614873'
+def translator(message):
+    message = message.replace(" ", "%20")
+    url = 'https://www.spanishdict.com/translate/' + message
+    driver.get(url)
+    time.sleep(2)
+    try:
+        results = driver.find_elements_by_xpath("//*[@class='translation--1A9t-']")
+        for result in results:
+            data = result.text
+        return data
+    except:
+        return "¡Lo siento, mi amigo! Please try again using a more complex sentence: this isn't for Spanish 1 homework!"
+
+#def func():
+    #page = requests.get(url, headers=headers)
+
+
+    #soup = BeautifulSoup(page.content, "lxml")
+    #data = soup.find(id="mt-Microsoft").media
+    #print(data)
+
+    #price = soup2.find(id="prcIsum", itemprop="price").get_text()
+
+    #converted_price = float(price[10:18].replace(',',''))
+    #converted_title = title[52:]
+
+    #print(converted_price)
+    #print(converted_title.strip())
+
+    #if converted_price < 1700: #replace with maximum price you want to be alerted for
+        #send_mail()
